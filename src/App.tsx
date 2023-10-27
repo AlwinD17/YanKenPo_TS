df98cb92d4746d64029726e3f6ef98c3a0f567eb
Alwin Dávila <edudavilaraffo@gmail.com>
2023-10-27 01:33:58 -0500

    First Yan-ken-po prototype

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,14 @@ import { ElementButton } from './Components/ElementButton.tsx'
 
 export function App() {
   
+  const randomEleIndex:number = Math.floor(Math.random()*3)
+  const resultado = (a:number | null, b:number) => {
+   if (typeof a === null) return (undefined)
+   else{
+    const formula:number = (a - b) % 3
+    return (formula === 0 ? 'Draw' : formula === 1 ? 'You win' : "You lose")  }
+  
+  } 
   const elements:{name: string, value: number, img: string}[] = [
     {   
         name:"Piedra",
@@ -12,20 +20,20 @@ export function App() {
         img:""
     },
     {
-        name:"Tijera",
-        value:2,
-        img:""
+      name:"Papel",
+      value:3,
+      img:""
     },
     {
-        name:"Papel",
-        value:3,
+        name:"Tijera",
+        value:2,
         img:""
     }
   ]
   const [activeButton, setActiveButton] = useState<number | null>(null);
   const jugadas:JSX.Element[] = elements.map((ele, index) => {
     return <ElementButton key={ele.value} element={ele} isActive = {activeButton === index} 
-                          onButtonClick={() => (activeButton === index) ? setActiveButton(null) : setActiveButton(index) } />});
+                          onButtonClick={() => (activeButton === index) ? setActiveButton(null) : setActiveButton(index) } /> }  );
   return (
     <>
       <div>
@@ -34,6 +42,14 @@ export function App() {
       <div>
       Elemento activo:
         {activeButton !== null ? elements[activeButton].name : 'Ninguno'}
+      </div>
+      <div>
+        {activeButton !== null && (
+        <div>
+          PC: {elements[randomEleIndex].name}
+          <br/>
+          Resultado: {resultado(elements[activeButton].value, elements[randomEleIndex].value) }
+        </div>) }
       </div>
     </>
   )
